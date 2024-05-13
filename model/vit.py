e81752c5968921b680faeb7e627c9acfd53cc011
九清秋 <103316169+Peyilo@users.noreply.github.com>
2024-05-13 16:16:00 +0800

    Add files via upload

---
+++ model/vit.py
@@ -0,0 +1,199 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+from PIL import Image
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange, Reduce
+from torch import Tensor
+from torch import nn
+from torchvision.transforms import Compose, Resize, ToTensor
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 768, img_size: int = 224):
+        self.patch_size = patch_size
+        super().__init__()
+        self.projection = nn.Sequential(
+            # using a conv layer instead of a linear one -> performance gains
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b e (h) (w) -> b (h w) e'),
+        )
+        self.cls_token = nn.Parameter(torch.randn(1, 1, emb_size))
+        self.positions = nn.Parameter(torch.randn((img_size // patch_size) ** 2 + 1, emb_size))
+
+    def forward(self, x: Tensor) -> Tensor:
+        b, _, _, _ = x.shape
+        x = self.projection(x)
+        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
+        # prepend the cls token to the input
+        x = torch.cat([cls_tokens, x], dim=1)
+        # add position embedding
+        x += self.positions
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, emb_size: int = 768, num_heads: int = 8, dropout: float = 0):
+        super().__init__()
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.keys = nn.Linear(emb_size, emb_size)
+        self.queries = nn.Linear(emb_size, emb_size)
+        self.values = nn.Linear(emb_size, emb_size)
+        self.att_drop = nn.Dropout(dropout)
+        self.projection = nn.Linear(emb_size, emb_size)
+        self.scaling = (self.emb_size // num_heads) ** -0.5
+
+    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
+        # split keys, queries and values in num_heads
+        queries = rearrange(self.queries(x), "b n (h d) -> b h n d", h=self.num_heads)
+        keys = rearrange(self.keys(x), "b n (h d) -> b h n d", h=self.num_heads)
+        values = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
+        # sum up over the last axis
+        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)  # batch, num_heads, query_len, key_len
+        if mask is not None:
+            fill_value = torch.finfo(torch.float32).min
+            energy.mask_fill(~mask, fill_value)
+
+        att = F.softmax(energy * self.scaling, dim=-1)
+        att = self.att_drop(att)
+        # sum up over the third axis
+        out = torch.einsum('bhal, bhlv -> bhav ', att, values)
+        out = rearrange(out, "b h n d -> b n (h d)")
+        out = self.projection(out)
+        return out
+
+
+class ResidualAdd(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x += res
+        return x
+
+
+class FeedForwardBlock(nn.Sequential):
+    def __init__(self, emb_size: int, expansion: int = 4, drop_p: float = 0.):
+        super().__init__(
+            nn.Linear(emb_size, expansion * emb_size),
+            nn.GELU(),
+            nn.Dropout(drop_p),
+            nn.Linear(expansion * emb_size, emb_size),
+        )
+
+
+class TransformerEncoderBlock(nn.Sequential):
+    def __init__(self,
+                 emb_size: int = 768,
+                 drop_p: float = 0.,
+                 forward_expansion: int = 4,
+                 forward_drop_p: float = 0.,
+                 **kwargs):
+        super().__init__(
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MultiHeadAttention(emb_size, **kwargs),
+                nn.Dropout(drop_p)
+            )),
+            ResidualAdd(nn.Sequential(
+                nn.LayerNorm(emb_size),
+                FeedForwardBlock(
+                    emb_size, expansion=forward_expansion, drop_p=forward_drop_p),
+                nn.Dropout(drop_p)
+            )
+            ))
+
+
+class TransformerEncoder(nn.Sequential):
+    def __init__(self, depth: int = 12, **kwargs):
+        super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
+
+
+class ClassificationHead(nn.Sequential):
+    def __init__(self, emb_size: int = 768, n_classes: int = 1000):
+        super().__init__(
+            Reduce('b n e -> b e', reduction='mean'),
+            nn.LayerNorm(emb_size),
+            nn.Linear(emb_size, n_classes))
+
+
+class ViT(nn.Sequential):
+    """
+    summary(ViT(), (3, 224, 224), device='cpu')
+    ================================================================
+    Total params: 86,415,592
+    Trainable params: 86,415,592
+    Non-trainable params: 0
+    ----------------------------------------------------------------
+    Input size (MB): 0.57
+    Forward/backward pass size (MB): 364.33
+    Params size (MB): 329.65
+    Estimated Total Size (MB): 694.56
+    ----------------------------------------------------------------
+    """
+
+    def __init__(self,
+                 in_channels: int = 3,
+                 patch_size: int = 16,
+                 emb_size: int = 768,
+                 img_size: int = 224,
+                 depth: int = 12,
+                 n_classes: int = 1000,
+                 **kwargs):
+        super().__init__(
+            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
+            TransformerEncoder(depth, emb_size=emb_size, **kwargs),
+            ClassificationHead(emb_size, n_classes)
+        )
+
+
+def load_data(batch_size: int, train=None, test=None):
+    train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+    test_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+    if train is None:
+        train = CIFAR10("D:\Code\Pycharm\stable-weight-decay-regularization\data", train=True, download=True,
+                        transform=train_transform)
+    if test is None:
+        test = CIFAR10("D:\Code\Pycharm\stable-weight-decay-regularization\data", train=False, download=True,
+                       transform=test_transform)
+
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test, batch_size=batch_size, shuffle=True)
+    return train_loader, test_loader
+
+
+if __name__ == "__main__":
+    load_data()
+    # img = Image.open('./cat.jpg')
+    #
+    # fig = plt.figure()
+    # plt.imshow(img)
+    # # resize to imagenet size
+    # transform = Compose([Resize((224, 224)), ToTensor()])
+    # x = transform(img)
+    # x = x.unsqueeze(0)  # add batch dim
+    # print(ViT(n_classes=10)(x).shape)
